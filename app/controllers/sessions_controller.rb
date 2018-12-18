902f3bc1a8ec4200915acaffa12469d050f06971
toshiyuki.igarashi <ged00475@nifty.com>
2018-12-18 09:43:43 +0000

    add 'how to use' page (#15)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,7 @@ class SessionsController < ApplicationController
 
     if login(email, password)
       flash[:success] = 'ログインに成功しました。'
-      #redirect_to @user
-      redirect_to root_url    # user controllerをインプリするまで、仮にrootを表示する
+      redirect_to new_diary_url
     else
       flash.now[:danger] = 'ログインに失敗しました。'
       render 'new'
