469be7f8df5f9abf1324bebc82ac34f5700c3b6e
toshiyuki.igarashi <ged00475@nifty.com>
2018-12-19 23:36:50 +0000

    display leading blank on diary show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  TAB_SIZE = 4
+  def count_leading_blank(str)
+    count = 0
+    (0..str.length - 1).each do |i|
+      case str[i]
+      when ' ' then
+        count += 1
+      when '　' then
+        count += 2
+      when "\t"
+        count = ((count + TAB_SIZE) / TAB_SIZE) * TAB_SIZE
+      else
+        return count
+      end
+    end
+    count
+  end
 end
