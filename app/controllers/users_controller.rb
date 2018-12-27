16ca826ae180c453b39869c167ec1b807d80677e
Toshiyuki Igarashi <ged00475@nifty.com>
2018-12-27 14:53:12 +0900

    create DiaryForm for new user (#22)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,9 @@ class UsersController < ApplicationController
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
       logged_in_session
+      if (!DiaryForm.new(user_id: @user.id, title: '日記帳', form: 'トピック,本文').save)
+        puts "**** Alert **** diary_form hasn't saved for user (user_id: #{@user.id})"
+      end
       redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
