50e922eadc965a94005b0ea80cc1639bb80d994f
Toshiyuki Igarashi <ged00475@nifty.com>
2018-12-27 14:09:34 +0900

    update 'create diary' function for filling form_id (#21)

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -82,7 +82,7 @@ class DiariesController < ApplicationController
   def prepare_picked_diary
     @diary = Diary.find_by(user_id: current_user.id, date_of_diary: picked_date)
     if @diary == nil
-      @diary = Diary.new(user_id: current_user.id, date_of_diary: picked_date)
+      @diary = Diary.new(user_id: current_user.id, date_of_diary: picked_date, form_id: DiaryForm.find_by(user_id: current_user.id).id)
       nil
     end
   end
