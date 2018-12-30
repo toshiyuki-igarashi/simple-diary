f06726b84be9c5afea6a1d1dfa5b5c4490549b4c
Toshiyuki Igarashi <ged00475@nifty.com>
2018-12-30 17:09:07 +0900

    add up/down/delete of form item

--- app/controllers/diary_forms_controller.rb
+++ app/controllers/diary_forms_controller.rb
@@ -5,13 +5,26 @@ class DiaryFormsController < ApplicationController
   end
 
   def update
-    if (params[:commit] == "項目の追加")
-      current_form[""] = { "文字数": "", "単位": "" }
-      current_diary_form.update(form: make_form(current_form))
-      redirect_to edit_diary_form_url(current_diary_form)
-    else
-      current_diary_form.update(form: make_form(construct_form(params)))
+    selected = []
+    items = construct_form(params, selected)
+
+    if (params[:commit] == "保存")
+      current_diary_form.update(form: make_form(items))
       redirect_to user_url(current_user)
+    else
+      if (params[:commit] == "追加")
+        form = insert_item(items, selected)
+      elsif (params[:commit] == "上")
+        form = move_up_item(items, selected)
+      elsif (params[:commit] == "下")
+        form = move_down_item(items, selected)
+      elsif (params[:commit] == "削除")
+        form = delete_item(items, selected)
+      else
+        form = items
+      end
+      current_diary_form.update(form: make_form(form))
+      redirect_to edit_diary_form_url(current_diary_form)
     end
   end
 
@@ -22,7 +35,7 @@ class DiaryFormsController < ApplicationController
         items[item_name][key] = value
   end
 
-  def construct_form(params)
+  def construct_form(params, selected)
     items = {}
     params.each do |key, value|
       if (key[0..2] == "項目名")
@@ -32,12 +45,81 @@ class DiaryFormsController < ApplicationController
       elsif (key[0..1] == "単位")
         save_item(items, "単位", key[2..-1], value)
       end
+
+      if (value == 'select')
+        selected.push(key)
+      end
     end
 
     form = {}
     items.each do |key, value|
       form[value["項目名"]] = {}
       form[value["項目名"]] = {"文字数": value["文字数"], "単位": value["単位"]}
+    end
+    form
+  end
+
+  def insert_item(items, selected)
+    form = {}
+    items.each do |key, value|
+      if selected.include?(key)
+        form[""] = { "文字数": "", "単位": "" }
+      end
+      form[key] = value
+    end
+
+    if (selected.size == 0)
+      form[""] = { "文字数": "", "単位": "" }
+    end
+    form
+  end
+
+  def copy_value(keys, original, target)
+    (0..keys.length-1).each do |i|
+      target[keys[i]] = original[keys[i]]
+    end
+  end
+
+  def move_up_item(items, selected)
+    form = {}
+    keys = items.keys
+    if (keys.length >= 2)
+      (1..keys.length-1).each do |i|
+        if selected.include?(keys[i])
+          temp = keys[i-1]
+          keys[i-1] = keys[i]
+          keys[i] = temp
+        end
+      end
+    end
+
+    copy_value(keys, items, form)
+    form
+  end
+
+  def move_down_item(items, selected)
+    form = {}
+    keys = items.keys
+    if (keys.length >= 2)
+      (keys.length-1).downto(1) do |i|
+        if selected.include?(keys[i-1])
+          temp = keys[i-1]
+          keys[i-1] = keys[i]
+          keys[i] = temp
+        end
+      end
+    end
+
+    copy_value(keys, items, form)
+    form
+  end
+
+  def delete_item(items, selected)
+    form = {}
+    items.each do |key, value|
+      if !selected.include?(key)
+        form[key] = value
+      end
     end
     form
   end
