e9678312757b74b28a906c8d0003158a3ea1c8b4
Toshiyuki Igarashi <ged00475@gmail.com>
2024-04-23 17:47:41 +0900

    add an arrow for goint to top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,15 @@
 //= require moment
 //= require moment/ja
 //= require bootstrap-datetimepicker
+//
+
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
+
+
